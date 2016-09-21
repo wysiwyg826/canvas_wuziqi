a1c8672ed1f705583757f36f6c8ad53b3b23b559
wysiwyg_826 <wysiwyg_826@163.com>
2016-09-21 14:39:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+require('./style.css');
+
+var chess = document.getElementById('chess');
+var context = chess.getContext("2d");
+
+for(var i = 0; i < 15; i++) {
+  context.moveTo(15 + i * 30, 15);
+  context.lineTo(15 + i * 30, 435);
+  context.stroke();
+  context.moveTo(15, 15 + i * 30);
+  context.lineTo(435, 15 + i * 30);
+  context.stroke();
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack')
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    // entry point of our application
+    entry: './index.js',
+    // where to place the compiled bundle
+    output: {
+        path: __dirname + '/dist',
+        filename: 'build.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        // `loaders` is an array of loaders to use.
+        // here we are only configuring vue-loader
+        loaders: [{
+            test: /\.html$/, // a regex for matching all files that end in `.vue`
+            loader: 'html' // loader to use for matched files
+        }, {
+            test: /\.css$/, // a regex for matching all files that end in `.vue`
+            loader: 'style!css' // loader to use for matched files
+        }]
+    }
+}
